0030553e76b14a1136ef3cb3abdfceda06c7191f
ray <shaochunpeng@gmail.com>
2016-10-28 04:58:19 -0400

    optimized blink and multiframe

--- player.d
+++
@@ -1,2 +0,0 @@
-player.o player.d : player.cpp player.h drawable.h vector2f.h frame.h gamedata.h \
- parseXML.h frameFactory.h
--- rotateSprite.d
+++
@@ -1,2 +0,0 @@
-rotateSprite.o rotateSprite.d : rotateSprite.cpp rotateSprite.h drawable.h vector2f.h \
- frame.h gamedata.h parseXML.h frameFactory.h
